3ac67584776b574859c2ed52b8f1bb238304e38c
kguan73611 <fcn2ay@virginia.edu>
2025-09-09 02:28:36 +0000

    Saving our two init files

---
+++ scripts/init.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install make -y
+sudo apy install python3.12-venv -y
+sudo apt install tree
---
+++ scripts/init_git_config.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=fcn2ay@virginia.edu
+NAME=kguan73611
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
